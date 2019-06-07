22e04135cb28e0f969e87cbba382bf1b83dbab3f
Fausto Bonacina <fausto.bonacina@bitrock.it>
2019-06-07 09:15:02 +0200

    clean code

--- src/main/scala/org/fostash/motorsettings/db/entities/RiderTimeTable.scala
+++
@@ -1,4 +0,0 @@
-package org.fostash.motorsettings.db.entities
-
-case class RiderTimeTable(id: Long, circuitId: Long, riderId: Long, bikeId: Long, sessionDate: String)
-case class RiderLap(id: Long, timeTableId: Long, lapNum: Int, minutes: Int, seconds: Int, millis: Int)
